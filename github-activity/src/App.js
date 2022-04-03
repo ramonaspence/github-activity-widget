580d242e95b43114a8d0e60506b884752c46b8c8
ramona <ramonaejspence@gmail.com>
2022-04-03 18:23:28 -0400

    added componentDidUpdate()

--- github-activity/src/App.js
+++ github-activity/src/App.js
@@ -9,13 +9,50 @@ class App extends Component {
     super();
 
     this.state = {
-      ghActivity: [],
+        'pushEvents': [],
+        'pullRequestReviewEvents': [],
+        'pullRequestEvents': [],
+        'issueCommentEvents': [],
+        'createEvents': [],
+        'deleteEvents': [],
     }
   }
 
   async componentDidMount() {
-    let response = await axios.get('https://api.github.com/users/ramonaspence/events');
-    console.log(response);
+    let response = await axios.get('https://api.github.com/users/ramonaspence/events')
+    for (let i = 0; i < response.data.length; i++) {
+      if (response.data[i]['type'] === 'PushEvent') {
+        let pushEvents = [...this.state['pushEvents']];
+        pushEvents.push(response.data[i]);
+        this.setState({pushEvents: pushEvents});
+        }
+      else if (response.data[i]['type'] === 'PullRequestReviewEvent' || response.data[i]['type'] ===  'PullRequestsReviewCommentEvent') {
+        let pullRequestReviewEvents = [...this.state['pullRequestReviewEvents']];
+        pullRequestReviewEvents.push(response.data[i]);
+        this.setState({pullRequestReviewEvents: pullRequestReviewEvents})
+        }
+      else if (response.data[i]['type'] === 'PullRequestEvent') {
+        let pullRequestEvents = [...this.state['pullRequestEvents']];
+        pullRequestEvents.push(response.data[i]);
+        this.setState({pullRequestEvents: pullRequestEvents})
+      }
+      else if (response.data[i]['type'] === 'IssueCommentEvent') {
+        let issueCommentEvents = [...this.state['issueCommentEvents']];
+        issueCommentEvents.push(response.data[i]);
+        this.setState({issueCommentEvents: issueCommentEvents})
+      }
+      else if (response.data[i]['type'] === 'CreateEvent') {
+        let createEvents = [...this.state['createEvents']];
+        createEvents.push(response.data[i]);
+        this.setState({createEvents: createEvents})
+      }
+      else if (response.data[i]['type'] === 'DeleteEvent') {
+        let deleteEvents = [...this.state['deleteEvents']];
+        deleteEvents.push(response.data[i]);
+        this.setState({deleteEvents: deleteEvents})
+      }
+    }
+    console.log('state', this.state);
   }
 
   render() {
